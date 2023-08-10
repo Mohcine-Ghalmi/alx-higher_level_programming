cb8992feac51ff2d4fb81d62b22107dba138d4b2
Mohcine Ghalmi <mghalmi@e2r7p7.1337.ma>
2023-08-10 21:22:26 +0100

    . islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
         if ord(c) >= ord('a') and ord(c) <= ord('z'):
-            print("True")
-        print("False")
+            return True
+        return False
