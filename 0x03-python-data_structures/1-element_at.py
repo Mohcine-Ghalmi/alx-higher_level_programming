4aa64751cdf45216c91b4056adacc82ce9fe3d4c
Mohcine Ghalmi <mghalmi@e2r6p14.1337.ma>
2023-08-12 09:03:09 +0100

    fixed

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,3 @@
 #!/user/bin/python3
 def element_at(my_list, idx):
-        if my_list[idx]:
-            return my_list[idx]
-        return "None"
+    return(my_list[idx] if 0 <= idx < len(my_list) else "None")
