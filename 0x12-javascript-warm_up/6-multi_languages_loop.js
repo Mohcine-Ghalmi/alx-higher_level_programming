dda36f4654f079dde70a6fa9f900f8e115a7e63a
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2023-11-13 17:28:18 +0100

    adding space

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const arg in args)
-  console.log(args[arg]);
+for (const arg in args){
+    console.log(args[arg]);
+}
