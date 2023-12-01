1401f00f4eb2c88a89dea65f5660f483e1d00106
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2023-12-01 15:50:53 +0100

    post an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+send POST req with an email 
+"""
+from sys import argv
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    data = urlencode(value).encode("ascii")
+    req = Request(url, data)
+
+    with urlopen(req) as response:
+        print(response.read().decode("utf-8", "replace"))
