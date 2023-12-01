839e5a25e1547b08be100e7d1dbabe97ca4d9a8c
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2023-12-01 19:38:40 +0100

    github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+My github
+"""
+from sys import argv
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(argv[1], argv[2])
+    req = requests.get("https://api.github.com/user", auth=auth)
+
+    print(req.json().get("id"))
