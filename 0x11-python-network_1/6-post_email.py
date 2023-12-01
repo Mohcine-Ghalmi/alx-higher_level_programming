02e1c52eca39ba4b7ffd94259da2b3fd1ca4edf6
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2023-12-01 19:33:51 +0100

    post email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+post a email
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    req = requests.post(url, data=value)
+
+    print(req.text)
